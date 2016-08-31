96060693f54b618f27701b3fd113d8b652e93373
srazafindrazaka <srazafindrazaka@valueit.fr>
2016-08-31 09:02:26 +0300

    fixing parent id

--- src/com/pushtech/crawler/launcher/Crawl.java
+++ src/com/pushtech/crawler/launcher/Crawl.java
@@ -25,131 +25,125 @@ import com.pushtech.crawler.serialization.ProductDAO;
  * Created by Workdev on 10/06/2016.
  */
 public class Crawl {
-	public Crawl(String entryPointUrl) {
-		// try {
-		logger.info("Begin crawl");
-		Page page = null;
-		String urlToConnect = entryPointUrl;
-		try {
-			page = getPageFromUrl(urlToConnect,
-					EngineContext.MethodType.GET_METHOD);
-			if (PageType.isProductPage(page)) {
-				offerCrawling(page, urlToConnect);
-			} else if (PageType.isListingPage(page)) {
-				listingCrawling(page);
-			}
+   public Crawl(String entryPointUrl) {
+      // try {
+      logger.info("Begin crawl");
+      Page page = null;
+      String urlToConnect = entryPointUrl;
+      try {
+         page = getPageFromUrl(urlToConnect, EngineContext.MethodType.GET_METHOD);
+         if (PageType.isProductPage(page)) {
+            offerCrawling(page, urlToConnect);
+         } else if (PageType.isListingPage(page)) {
+            listingCrawling(page);
+         }
 
-			else if (PageType.isHomePage(page)) {
-				homeCrawling(page);
-			}
-		} catch (Exception e) {
-			SwingUtilities.invokeLater(new Runnable() {
-				@Override
-				public void run() {
-					JOptionPane jop = new JOptionPane();
-					jop.showMessageDialog(null, "Crawl failed", "Error",
-							JOptionPane.ERROR_MESSAGE);
-				}
-			});
-			logger.fatal(e.getMessage());
-			e.printStackTrace();
-		}
-		SwingUtilities.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				JOptionPane jop = new JOptionPane();
-				jop.showMessageDialog(null, "Crawl ended", "Information",
-						JOptionPane.INFORMATION_MESSAGE);
-			}
-		});
-		logger.info("Crawl ended");
-	}
+         else if (PageType.isHomePage(page)) {
+            homeCrawling(page);
+         }
+      } catch (Exception e) {
+         SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+               JOptionPane jop = new JOptionPane();
+               jop.showMessageDialog(null, "Crawl failed", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+         });
+         logger.fatal(e.getMessage());
+         e.printStackTrace();
+      }
+      SwingUtilities.invokeLater(new Runnable() {
+         @Override
+         public void run() {
+            JOptionPane jop = new JOptionPane();
+            jop.showMessageDialog(null, "Crawl ended", "Information", JOptionPane.INFORMATION_MESSAGE);
+         }
+      });
+      logger.info("Crawl ended");
+   }
 
-	public static Page getPageFromUrl(final String url,
-			EngineContext.MethodType methodeType) {
-		Page page = null;
-		HttpResponse response = null;
-		HashMap<String, String> headers = new HashMap<String, String>();
+   public static Page getPageFromUrl(final String url, EngineContext.MethodType methodeType) {
+      Page page = null;
+      HttpResponse response = null;
+      HashMap<String, String> headers = new HashMap<String, String>();
 
-		headers.put("Host", "www.wordans.fr");
-		headers.put("User-Agent",
-				"Mozilla/5.0 (Windows NT 10.0; WOW64; rv:43.0) Gecko/20100101 Firefox/43.0");
+      headers.put("Host", "www.wordans.fr");
+      headers.put("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:43.0) Gecko/20100101 Firefox/43.0");
 
-		response = ConnectionHandler.getResponse(url, null, headers,
-				methodeType);
-		page = (Page) ParserFactory.getAppropriateParsingTemplate(response)
-				.parse(url, response, null);
+      response = ConnectionHandler.getResponse(url, null, headers, methodeType);
+      page = (Page) ParserFactory.getAppropriateParsingTemplate(response).parse(url, response, null);
 
-		return page;
-	}
+      return page;
+   }
 
-	private void offerCrawling(Page page, String productPath) {
-		Product product = new CrawlOffer().doAction(page);
-		System.out.println("Link : " + productPath);
+   private void offerCrawling(Page page, String productPath) {
+      Product product = new CrawlOffer().doAction(page);
+      System.out.println("Link : " + productPath);
 
-		// String productId = null;
-		// try {
-		// productId = getProductIdFromLink(productPath);
-		// } catch (Exception e) {
-		// logger.error("Error on getting Id from link");
-		// }
-		// logger.debug("Product Id : " + productId);
-		// product.setId(productId);
+      // String productId = null;
+      // try {
+      // productId = getProductIdFromLink(productPath);
+      // } catch (Exception e) {
+      // logger.error("Error on getting Id from link");
+      // }
+      // logger.debug("Product Id : " + productId);
+      // product.setId(productId);
 
-		product.setLink(productPath);
-		// product.setId(productId);
+      product.setLink(productPath);
+      // product.setId(productId);
 
-		DAOFactory daoFactory = new DataBaseDAO().getFactoryInstance();
-		AbstractDAOEntity daoEntity = new ProductDAO(daoFactory);
-		VariantParser variantExtractor = VariantParser.getExtractor(product,
-				page);
-		ArrayList<Product> variantList = variantExtractor.doAction(page);
-		for (Product variantProduct : variantList) {
-			daoEntity.updateEntity(variantProduct);
-		}
+      DAOFactory daoFactory = new DataBaseDAO().getFactoryInstance();
+      AbstractDAOEntity daoEntity = new ProductDAO(daoFactory);
+      VariantParser variantExtractor = VariantParser.getExtractor(product, page);
+      ArrayList<Product> variantList = variantExtractor.doAction(page);
+      for (Product variantProduct : variantList) {
+         daoEntity.updateEntity(variantProduct);
 
-	}
+      }
+      if (variantList.size() > 0) {
+         Product ProductElement = variantList.get(0);
+         ProductElement.setId(ProductElement.getParentId());
+         daoEntity.updateEntity(ProductElement);
+      }
 
-	private void homeCrawling(Page homePage) {
-		ArrayList<String> allListing = CrawlHome.getAllListing(homePage);
-		for (String listing : allListing) {
-			Page listingPage = getPageFromUrl(listing,
-					EngineContext.MethodType.GET_METHOD);
-			listingCrawling(listingPage);
-		}
-	}
+   }
 
-	private String listingProcess(Page listingPage) {
-		int indexProduit = 0;
-		for (String productPath : CrawlListing.getProductLinks(listingPage)) {
-			System.out.println("-------------------- Produit n* "
-					+ indexProduit + " --------------------");
+   private void homeCrawling(Page homePage) {
+      ArrayList<String> allListing = CrawlHome.getAllListing(homePage);
+      for (String listing : allListing) {
+         Page listingPage = getPageFromUrl(listing, EngineContext.MethodType.GET_METHOD);
+         listingCrawling(listingPage);
+      }
+   }
 
-			try {
-				Page productPage = getPageFromUrl(productPath,
-						EngineContext.MethodType.GET_METHOD);
-				offerCrawling(productPage, productPath);
-				// System.out.println("-------------");
-				indexProduit++;
-				// break;
-			} catch (Exception e) {
-				logger.error("" + e.getMessage());
-			}
-		}
-		return getNextPageLink(listingPage.getDoc());
-	}
+   private String listingProcess(Page listingPage) {
+      int indexProduit = 0;
+      for (String productPath : CrawlListing.getProductLinks(listingPage)) {
+         System.out.println("-------------------- Produit n* " + indexProduit + " --------------------");
 
-	private void listingCrawling(Page firstListingPage) {
-		boolean continueCrawl = true;
-		Page page = firstListingPage;
-		String nextPageLink = null;
-		while (continueCrawl) {
-			nextPageLink = listingProcess(page);
-			continueCrawl = nextPageLink != null ? true : false;
-			if (continueCrawl) {
-				page = getPageFromUrl(nextPageLink,
-						EngineContext.MethodType.GET_METHOD);
-			}
-		}
-	}
+         try {
+            Page productPage = getPageFromUrl(productPath, EngineContext.MethodType.GET_METHOD);
+            offerCrawling(productPage, productPath);
+            // System.out.println("-------------");
+            indexProduit++;
+            // break;
+         } catch (Exception e) {
+            logger.error("" + e.getMessage());
+         }
+      }
+      return getNextPageLink(listingPage.getDoc());
+   }
+
+   private void listingCrawling(Page firstListingPage) {
+      boolean continueCrawl = true;
+      Page page = firstListingPage;
+      String nextPageLink = null;
+      while (continueCrawl) {
+         nextPageLink = listingProcess(page);
+         continueCrawl = nextPageLink != null ? true : false;
+         if (continueCrawl) {
+            page = getPageFromUrl(nextPageLink, EngineContext.MethodType.GET_METHOD);
+         }
+      }
+   }
 }
